808719781d8e8d3a3948b5d17ea817ffa77d3993
Derek Wen <realderekwen2024@gmail.com>
2026-08-29 10:25:45 +1000

    Set up colour changing ability between night and day

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 console.log("Test to ensure it's up")
 // grab element from the page
-const btn = document.getElementById("btn")
+const btn = document.getElementById("toggle")
+// note the button id is toggle
 
 // run function when clicked
 btn.addEventListener("click", () => {
