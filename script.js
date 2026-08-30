7ed3d34c8388b372cd9ca4aef310d1f5bfeb2553
Derek Wen <realderekwen2024@gmail.com>
2026-08-30 15:47:02 +1000

    Added the sidebar

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 
 console.log("Test to ensure it's up")
+
 // grab element from the page
 const btn = document.getElementById("toggle")
 // note the button id is toggle
@@ -10,3 +11,21 @@ btn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 })
 
+const links = document.querySelectorAll(".nav-link");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    // Hide all sections initially
+    document.querySelectorAll(".page").forEach((page) => {
+      page.classList.remove("active");
+    });
+    
+    // Show the section that matches the request
+    document.getElementById(link.dataset.target).classList.add("active");
+
+    // Update which nav button is selected
+    links.forEach((link) => link.classList.remove("current"));
+    link.classList.add("current");
+
+  });
+});
